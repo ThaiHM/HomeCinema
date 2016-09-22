d03f1235ab9d575d1a37559f8db50256dd77807d
ThaiHM <hoangminhthai@outlook.com>
2016-09-22 15:24:36 +0700

    Add repository

---
+++ HomeCinema.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,14 @@
+﻿using HomeCinema.Data.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Data.Infrastructure
+{
+    public interface IDbFactory : IDisposable
+    {
+        HomeCinemaContext Init();
+    }
+}
