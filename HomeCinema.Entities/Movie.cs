b4ed1ce014a28c2e306b13b7af3fb76bd834f452
ThaiHM <hoangminhthai@outlook.com>
2016-09-25 22:05:15 +0700

    Update Movie.cs

--- HomeCinema.Entities/Movie.cs
+++ HomeCinema.Entities/Movie.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HomeCinema.Entities
 {
-    //ThaiHM 
+    //ThaiHM chinh sua
     public class Movie : IEntityBase
     {
         public Movie() { Stocks = new List<Stock>(); }
