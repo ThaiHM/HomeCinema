13164e9dbe190ce3bba023d1209ab83e7d167516
ThaiHM <hoangminhthai@outlook.com>
2016-10-24 16:37:57 +0700

    Add Service project

--- HomeCinema.Data/Configurations/CustomerConfiguration.cs
+++ HomeCinema.Data/Configurations/CustomerConfiguration.cs
@@ -14,7 +14,8 @@ namespace HomeCinema.Data.Configurations
             Property(u => u.FirstName).IsRequired().HasMaxLength(100);
             Property(u => u.LastName).IsRequired().HasMaxLength(100);
             Property(u => u.IdentityCard).IsRequired().HasMaxLength(50);
-            Property(u => u.UniqueKey).IsRequired(); Property(c => c.Mobile).HasMaxLength(10);
+            Property(u => u.UniqueKey).IsRequired(); 
+            Property(c => c.Mobile).HasMaxLength(10);
             Property(c => c.Email).IsRequired().HasMaxLength(200);
             Property(c => c.DateOfBirth).IsRequired();
         }
